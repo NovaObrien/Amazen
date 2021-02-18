b2b7f0a2cec8ae0ec008691ade273367dd367f41
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-02-18 16:40:04 -0700

    Updated tags on deploy,sh

--- amazen-server/deploy.sh
+++ amazen-server/deploy.sh
@@ -1,6 +1,6 @@
 dotnet publish -c Release
-docker build -t keepr-1 ./bin/Release/net5.0/publish
-docker tag keepr-1 registry.heroku.com/keepr-1/web
+docker build -t amazen-server ./bin/Release/net5.0/publish
+docker tag amazen-server registry.heroku.com/keepr-1/web
 docker push registry.heroku.com/keepr-1/web
 heroku container:release web -a keepr-1
 echo press any key
