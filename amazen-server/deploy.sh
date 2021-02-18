ad6acc7a26c34fdce5d954c1faa60c778c1a9bf0
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-02-18 14:59:44 -0700

    Update

--- amazen-server/deploy.sh
+++
@@ -1,7 +0,0 @@
-dotnet publish -c Release
-docker build -t TAG_NAME ./bin/Release/net5.0/publish
-docker tag TAG_NAME registry.heroku.com/HEROKU_APP_NAME/web
-docker push registry.heroku.com/HEROKU_APP_NAME/web
-heroku container:release web -a HEROKU_APP_NAME
-echo press any key
-read end
